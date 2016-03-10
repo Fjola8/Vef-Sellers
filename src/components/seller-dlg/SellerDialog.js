6d3132ad9b1bd1cbdc1f42323a979767392839d8
Fjola8 <fjola14@ru.is>
2016-03-10 19:36:37 +0000

    edit coolio

--- src/components/seller-dlg/SellerDialog.js
+++ src/components/seller-dlg/SellerDialog.js
@@ -3,10 +3,17 @@
 angular.module("project3App").factory("SellerDialog",
     function SellerDialog($uibModal) {
         return {
-            show: function() {
+            show: function show(seller) {
                 var modalInstance = $uibModal.open({
                     templateUrl: "components/seller-dlg/sellerDialog.html",
-                    controller: "SellerDialogController"
+                    controller: "SellerDialogController",
+                    resolve: {
+                        modalParam: function() {
+                            return {
+                                seller: seller
+                            };
+                        }
+                    }
                 });
                 return modalInstance.result;
             }
--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -23,15 +23,13 @@
 		});
 	};
 
-    $scope.onEditSeller = function onEditSeller(sellerID) {
-        SellerDialog.show().then(function(seller) {
-            AppResource.updateSeller(sellerID, seller).success(function(seller) {
-                centrisNotify.success("sellers.Messages.SaveSucceeded");
-            }).erro(function() {
-                centrisNotify.error("sellers.Messages.SaveFailed");
+    $scope.onEditSeller = function onEditSeller(sellerEdit) {
+        SellerDialog.show(sellerEdit).then(function(seller) {
+            AppResource.updateSeller(sellerEdit.id, seller).success(function(seller) {
+                centrisNotify.success("sellers.Messages.EditSucceeded");
+            }).error(function() {
+                centrisNotify.error("sellers.Messages.EditFailed");
             });
         });
     };
 });
-
-// EDIT AN ENTITY:  SellerDialog.show().then(function(modiefiedEntity))
