47650dc3be77e181ada28896a9f40f41b354001e
Fjola8 <fjola14@ru.is>
2016-03-14 21:12:47 +0000

    karma.confi fix og reyna unit test

--- src/components/sellers/SellersController.spec.js
+++ src/components/sellers/SellersController.spec.js
@@ -1,5 +1,47 @@
 "use strict";
 
-describe("SellersController should be unit tested here", function() {
-	// TODO: add beforeEach/describe/it/etc. functions as appropriate!
+describe("SellersController", function() {
+	beforeEach(module("project3App"));
+
+	var scope, ctrl, translate, resource, sellerDlg;
+
+	describe("Language checking", function() {
+		beforeEach(inject(function($controller, $rootScope, $translate) {
+			scope = $rootScope.$new();
+			translate = $translate;
+			ctrl = $controller("SellersController", {
+				$scope : scope,
+				$translate : translate
+			});
+		}));
+
+		it("should prefer is", function() {
+			scope.changeLanguage("");
+			expect(translate.proposedLanguage()).toEqual("is");
+		});
+
+		it("should change to is", function() {
+			scope.changeLanguage("is");
+			expect(translate.proposedLanguage()).toEqual("is");
+		});
+
+		it("should change to en", function() {
+			scope.changeLanguage("en");
+			expect(translate.proposedLanguage()).toEqual("en");
+		});
+	});
+
+/*	describe("checking sellers", function() {
+		beforeEach(inject(function($controller, $rootScope, AppResource, SellerDialog) {
+			scope = $rootScope.$new();
+			resource = AppResource;
+			sellerDlg = SellerDialog;
+			ctrl = $controller("SellersController", {
+				$scope : scope,
+				AppResource: resource,
+				SellerDialog : sellerDlg
+			});
+		}));
+	}); */
+
 });
